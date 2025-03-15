ab8fa86204631d4dc11dec48b8697c9943d1c683
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-03-15 09:37:09 +0600

    new package created

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import messaging from "@react-native-firebase/messaging";
 
 export default function App() {
+  async function requestUserPermission() {
+    console.log("first");
+    const authStatus = await messaging().requestPermission();
+    console.log("authStatus", JSON.stringify(authStatus, null, 2));
+    const enabled = authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    console.log("enabled", JSON.stringify(enabled, null, 2));
+    if (enabled) {
+      console.log("Authorization status:", authStatus);
+    }
+  }
+
+  useEffect(() => {
+    // requestUserPermission();
+    // if (requestUserPermission()) {
+    // } else {
+    //   console.log("Failed to get permission:", authStatus);
+    // }
+
+    console.log("Function called");
+    requestUserPermission();
+    return () => {};
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
